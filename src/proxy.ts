b99c91fc5d348c841b2d3e869fded96b5246b38d
Krypton321123 <coolrajsharma28@gmail.com>
2026-04-15 12:05:14 +0530

    new stuff for hosting

---
+++ src/proxy.ts
@@ -0,0 +1,27 @@
+import https from "https";
+import fs from "fs";
+import httpProxy from "http-proxy";
+import { IncomingMessage, ServerResponse } from "http";
+
+const proxy = httpProxy.createProxyServer({
+  target: "http://localhost:8000",
+  changeOrigin: true,
+});
+
+const options = {
+  key: fs.readFileSync("E:/Mobile-app-backend/api.saavlioils.com-key.pem"),
+  cert: fs.readFileSync("E:/Mobile-app-backend/api.saavlioils.com-chain.pem"),
+} as https.ServerOptions;
+
+https
+  .createServer(options, (req: IncomingMessage, res: ServerResponse) => {
+    // @ts-ignore
+    proxy.web(req, res, (err) => {
+      console.error("Proxy error:", err);
+      res.writeHead(500);
+      res.end("Something went wrong.");
+    });
+  })
+  .listen(443, () => {
+    console.log("🚀 HTTPS Proxy running on port 443");
+  });
